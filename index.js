0051455812bb25a73a7b63f23665324a6ac79712
Liatu Stephen <stephenliatu@gmail.com>
2022-10-05 16:48:54 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gym website</title>
+    <a href="styleseet" rel="gym.css">
+</head>
+<body>
+    
+    <div class="logo">
+        <h4>MY GYM WEBSITE</h4>
+    </div>
+    <ul class="nav-links">
+        <li>
+            <a href="a">HOME</a>
+        </li>
+        <li>
+            <a href="">About</a>
+        </li>
+        <li>
+            <a href="">Work</a>
+        </li>
+        <li>
+            <a href="">Services</a>
+        </li>
+    </ul>
+       <div class="burger">
+           <div class="link1">
+               <div class="link2">
+               </div>
+               <div class="link3">
+            </div>
+           </div>
+       </div> 
+       </nav>
+       <marquee behavior="left" direction="down">
+           <img src="./image-G1.jpeg" alt>
+           <img src="./image-G2.jpeg" alt>
+           <img src="./image-G3.jpeg" alt>
+
+       </marquee>
+       <div aria-colspan="nav">
+           <maquee behavior="left" direction="right">              
+           </maquee>
+           <marquee style="color: blue;">WELCOME MY GYM WEBSITE</marquee>
+       </div>
+       <div class="container">
+           <h3>lates news on Gym Pavilion</h3>
+           <h1>Events</h1>
+       </div>
+       <div class="topnav">
+           <input type="text"placeholder="search..">
+       </div>
+
+        
+
+    
+</body>
+</html>
